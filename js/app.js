b82a13fbae2da4fcd59968c045c437a87abd557c
bajo3 <Felipelentini98@hotmail.com>
2023-06-07 11:36:52 -0300

    Cierre de menu

--- js/app.js
+++ js/app.js
@@ -1,10 +1,18 @@
 $(document).ready(function() {
   const hamburger = document.querySelector('.hamburger');
   const menu = document.querySelector('.menu');
-
+  const links = document.querySelectorAll('.menu a');
+  
   hamburger.addEventListener('click', function() {
     menu.classList.toggle('open');
   });
+  
+  links.forEach(function(link) {
+    link.addEventListener('click', function() {
+      menu.classList.remove('open');
+    });
+  });
+  
 
   $('.carousel').slick({
     autoplay: true, // Agregado: Inicia el carousel automáticamente
