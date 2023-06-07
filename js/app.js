97b0483a32db9b8cd0285b5abe780409d1a8f352
bajo3 <Felipelentini98@hotmail.com>
2023-06-07 11:07:36 -0300

    Autplay carousel

--- js/app.js
+++ js/app.js
@@ -1,35 +1,32 @@
-
 $(document).ready(function() {
   const hamburger = document.querySelector('.hamburger');
   const menu = document.querySelector('.menu');
-  
+
   hamburger.addEventListener('click', function() {
     menu.classList.toggle('open');
   });
 
-    $('.carousel').slick({
-      autoplay: true,
-      autoplaySpeed: 2000,
-      infinite: true,
-      dots: true,
-      fade: true,
-      cssEase: 'linear',
-      responsive: [
-        {
-          breakpoint: 768,
-          settings: {
-            arrows: false
-          }
-        },
-        {
-          breakpoint: 480,
-          settings: {
-            arrows: false,
-            dots: false
-          }
+  $('.carousel').slick({
+    autoplay: true, // Agregado: Inicia el carousel automáticamente
+    autoplaySpeed: 2000,
+    infinite: true,
+    dots: true,
+    fade: true,
+    cssEase: 'linear',
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          arrows: false
         }
-      ]
-    });
-  
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          arrows: false,
+          dots: false
+        }
+      }
+    ]
+  });
 });
-
