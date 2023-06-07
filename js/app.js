9d2e7609a3754b953b6d72be41cc5df1474a02c2
bajo3 <Felipelentini98@hotmail.com>
2023-06-07 10:40:49 -0300

    Carousel

--- js/app.js
+++ js/app.js
@@ -1,6 +1,37 @@
-const hamburger = document.querySelector('.hamburger');
-const menu = document.querySelector('.menu');
 
-hamburger.addEventListener('click', function() {
-  menu.classList.toggle('open');
+$(document).ready(function() {
+  const hamburger = document.querySelector('.hamburger');
+  const menu = document.querySelector('.menu');
+  
+  hamburger.addEventListener('click', function() {
+    menu.classList.toggle('open');
+  });
+
+  $(document).ready(function() {
+    $('.carousel').slick({
+      autoplay: true,
+      autoplaySpeed: 3000,
+      infinite: true,
+      dots: true,
+      fade: true,
+      cssEase: 'linear',
+      responsive: [
+        {
+          breakpoint: 768,
+          settings: {
+            arrows: false
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            arrows: false,
+            dots: false
+          }
+        }
+      ]
+    });
+  });
+  
 });
+
